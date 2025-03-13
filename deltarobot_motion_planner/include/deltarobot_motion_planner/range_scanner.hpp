20a91064890bfd0947b7a8eaacd70cc765a792cf
Sharwin Patil <sharwin24@gmail.com>
2025-03-13 18:24:13 -0500

    Wrote scanning process

--- deltarobot_motion_planner/include/deltarobot_motion_planner/range_scanner.hpp
+++ deltarobot_motion_planner/include/deltarobot_motion_planner/range_scanner.hpp
@@ -1,5 +1,5 @@
-#ifndef SCANNING_HPP_
-#define SCANNING_HPP_
+#ifndef RANGE_SCANNER_HPP_
+#define RANGE_SCANNER_HPP_
 
 #include "rclcpp/rclcpp.hpp"
 #include "deltarobot_interfaces/msg/delta_joints.hpp"
@@ -14,21 +14,35 @@ using DeltaJoints = deltarobot_interfaces::msg::DeltaJoints;
 using RobotConfig = deltarobot_interfaces::msg::RobotConfig;
 using PlayDemoTrajectory = deltarobot_interfaces::srv::PlayDemoTrajectory;
 
-class Scanning : public rclcpp::Node {
+typedef struct {
+  double x;
+  double y;
+  double z;
+  double range;
+} ScanPoint;
+
+class RangeScanner : public rclcpp::Node {
 public:
 
-  Scanning();
-  ~Scanning() = default;
+  RangeScanner();
+  ~RangeScanner() = default;
 
 private:
   rclcpp::Client<PlayDemoTrajectory>::SharedPtr play_demo_trajectory_client;
   rclcpp::Subscription<RobotConfig>::SharedPtr robot_config_sub;
   rclcpp::Subscription<Range>::SharedPtr range_sub;
+  rclcpp::TimerBase::SharedPtr scanning_timer;
+
+  void startScanning();
 
   // Recent values obtained from subscriptions
   std::unique_ptr<RobotConfig> robot_config;
   std::unique_ptr<Range> range;
 
+  // Scanning parameters
+  std::vector<ScanPoint> scan_points;
+
+  bool isScanning = false;
 };
 
-#endif  // SCANNING_HPP_
+#endif  // RANGE_SCANNER_HPP_
